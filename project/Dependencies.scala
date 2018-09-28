1879e4829aacaa7f22a60ecce7d31a816c64dd2c
Gabriel Volpe <volpegabriel@gmail.com>
2018-09-28 10:22:14 +0900

    Upgrade to latest http4s M3. Bumping version.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,9 +4,9 @@ object Dependencies {
 
   object Versions {
     val catsEffect  = "1.0.0"
-    val fs2         = "1.0.0-M5"
-    val http4s      = "0.19.0-M2"
-    val circe       = "0.10.0-M2"
+    val fs2         = "1.0.0-RC1"
+    val http4s      = "0.19.0-M3"
+    val circe       = "0.10.0"
     val gfcTimeuuid = "0.0.8"
 
     // Test
