bd050fcae50f0fef621e617bd9a5e1b4926ce8df
Scala steward <me@scala-steward.org>
2019-02-19 21:06:10 +0100

    Update http4s-blaze-client, ... to 0.20.0-M6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val catsPar     = "0.2.1"
     val catsEffect  = "1.2.0"
     val fs2         = "1.0.3"
-    val http4s      = "0.20.0-M5"
+    val http4s      = "0.20.0-M6"
     val circe       = "0.11.1"
     val gfcTimeuuid = "0.0.8"
     val log4Cats    = "0.3.0"
