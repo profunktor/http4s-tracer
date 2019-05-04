a456034c8dfdd9745d58736a0f0fd18e24d2912b
Gabriel Volpe <volpegabriel@gmail.com>
2019-05-04 13:16:36 +0200

    Circle CI build + sbt-tpolecat plugin

--- examples/src/main/scala/com/github/gvolpe/tracer/http/AuthRoutes.scala
+++ examples/src/main/scala/com/github/gvolpe/tracer/http/AuthRoutes.scala
@@ -17,10 +17,12 @@
 package com.github.gvolpe.tracer.http
 
 import cats.effect.Sync
+import cats.syntax.flatMap._
 import com.github.gvolpe.tracer.Trace._
 import com.github.gvolpe.tracer.Tracer
 import com.github.gvolpe.tracer.algebra.UserAlgebra
 import com.github.gvolpe.tracer.auth.{AuthTracedHttpRoute, Http4sAuthTracerDsl}
+import com.github.gvolpe.tracer.model.user.Username
 import io.circe.generic.auto._
 import org.http4s._
 import org.http4s.server.{AuthMiddleware, Router}
@@ -31,7 +33,7 @@ class AuthRoutes[F[_]: Sync: Tracer](users: UserAlgebra[Trace[F, ?]]) extends Ht
 
   private val httpRoutes: AuthedService[String, F] = AuthTracedHttpRoute[String, F] {
     case GET -> Root as user using traceId =>
-      Ok(user -> traceId)
+      users.find(Username(user)).run(traceId) >> Ok(user -> traceId)
 
     case POST -> Root as user using traceId =>
       Created(user -> traceId)
