43352595891e041159628fcbcc225823267a1716
Scala Steward <me@scala-steward.org>
2019-10-31 06:39:15 +0100

    Update zio-interop-cats to 2.0.0.0-RC7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     val gfcTimeuuid = "0.0.8"
     val log4Cats    = "1.0.1"
     val zio         = "1.0.0-RC16"
-    val zioCats     = "2.0.0.0-RC6"
+    val zioCats     = "2.0.0.0-RC7"
 
     // Test
     val scalaTest  = "3.0.8"
