3e11fdb65a00d97c50c4f3b127bbd25d914267df
Scala Steward <me@scala-steward.org>
2019-12-02 09:10:10 +0100

    Update circe-core, circe-generic to 0.12.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val catsEffect  = "2.0.0"
     val fs2         = "2.1.0"
     val http4s      = "0.21.0-M6"
-    val circe       = "0.12.2"
+    val circe       = "0.12.3"
     val gfcTimeuuid = "0.0.8"
     val log4Cats    = "1.0.1"
     val zio         = "1.0.0-RC17"
