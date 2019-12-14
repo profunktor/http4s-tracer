073c05f5d38128359793cabebeb01f1cf6f79625
Scala Steward <me@scala-steward.org>
2019-12-14 14:36:20 +0100

    Update scalacheck to 1.14.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
 
     // Test
     val scalaTest  = "3.0.8"
-    val scalaCheck = "1.14.2"
+    val scalaCheck = "1.14.3"
 
     // Compiler
     val kindProjector    = "0.10.3"
