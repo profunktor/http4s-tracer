6bbf3ae036de9d54dde2277d47823ea9c37c5831
Scala Steward <me@scala-steward.org>
2020-02-08 00:54:34 +0100

    Update cats-effect to 2.1.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    val catsEffect  = "2.1.0"
+    val catsEffect  = "2.1.1"
     val fs2         = "2.2.2"
     val http4s      = "0.21.0-RC4"
     val circe       = "0.12.2"
