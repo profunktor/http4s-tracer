8333b2185ffee24bba66f62f9da9deeaf9d398d1
Scala Steward <me@scala-steward.org>
2020-02-09 22:44:44 +0100

    Update http4s-blaze-client, ... to 0.21.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   object Versions {
     val catsEffect  = "2.1.1"
     val fs2         = "2.2.2"
-    val http4s      = "0.21.0-RC5"
+    val http4s      = "0.21.0"
     val circe       = "0.12.2"
     val gfcTimeuuid = "0.0.8"
     val log4Cats    = "1.0.1"
