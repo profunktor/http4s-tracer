c0b2cef4cde2e1a7e2dc4c8d47fa8f2b2f0bc0bb
Scala Steward <me@scala-steward.org>
2020-03-20 20:28:13 +0100

    Update fs2-core to 2.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   object Versions {
     val catsEffect  = "2.1.2"
-    val fs2         = "2.2.2"
+    val fs2         = "2.3.0"
     val http4s      = "0.21.1"
     val circe       = "0.13.0"
     val gfcTimeuuid = "0.0.8"
