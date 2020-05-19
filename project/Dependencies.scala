641a669d4c2b047047f6847f9df5e4bb464c29f0
Scala Steward <me@scala-steward.org>
2020-05-19 14:55:06 +0200

    Update log4cats-core, log4cats-slf4j to 1.1.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val http4s      = "0.21.3"
     val circe       = "0.13.0"
     val gfcTimeuuid = "0.0.8"
-    val log4Cats    = "1.0.1"
+    val log4Cats    = "1.1.1"
     val zio         = "1.0.0-RC19-1"
     val zioCats     = "2.0.0.0-RC14"
 
