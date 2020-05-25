98852b5776093b310f7698a9d3f3fdce385de9c4
Scala Steward <me@scala-steward.org>
2020-05-26 00:42:42 +0200

    Update zio to 1.0.0-RC20

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val circe       = "0.13.0"
     val gfcTimeuuid = "0.0.8"
     val log4Cats    = "1.1.1"
-    val zio         = "1.0.0-RC19-2"
+    val zio         = "1.0.0-RC20"
     val zioCats     = "2.0.0.0-RC14"
 
     // Test
