af776daf7b192377837b0b016b1ad9632938cb8e
Scala Steward <me@scala-steward.org>
2020-07-25 12:39:35 +0200

    Update munit, munit-scalacheck to 0.7.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     val zioCats     = "2.0.0.0-RC14"
 
     // Test
-    val munit = "0.7.9"
+    val munit = "0.7.10"
 
     // Compiler
     val kindProjector    = "0.11.0"
