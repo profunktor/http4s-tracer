f977167ba4701ab4c3aaa29500e15dd528264376
Scala Steward <me@scala-steward.org>
2021-06-02 08:48:47 +0200

    Update zio to 1.0.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val circe       = "0.13.0"
     val gfcTimeuuid = "0.0.8"
     val log4Cats    = "1.1.1"
-    val zio         = "1.0.7"
+    val zio         = "1.0.9"
     val zioCats     = "2.4.1.0"
 
     // Test
